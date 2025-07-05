0f9cbdff7319e101f4a321e3b2cdef12dc3ed01c
samfinkelstein <finkelstein.sam@btinternet.com>
2025-07-05 14:26:35 +0100

    speech extraction function

--- PartTwo.py
+++ PartTwo.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn.feature_extraction import TfidfVectorizer
 def readhansard():
     df = pd.read_csv("p2-texts/hansard40000.csv")
     return df
@@ -35,3 +35,18 @@ df3 = fourpartysystem(df)
 df4 = speechonly(df)
 df5 = noshortspeeches(df)
 print(dimensions(df5))
+
+'''
+Vectorise the speeches using TfidfVectorizer from scikit-learn. Use the default
+parameters, except for omitting English stopwords and setting max_features to
+3000. Split the data into a train and test set, using stratified sampling, with a
+random seed of 26.
+'''
+def speechextracter(df):
+    speechlist = df['speech'].values.tolist()
+    return speechlist
+    
+def vectorize():
+    #traindata = 
+    #testdata =
+    pass
