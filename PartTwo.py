d09c4a622f088d036eee9be2ef6228f3012020db
samfinkelstein <finkelstein.sam@btinternet.com>
2025-07-06 13:22:26 +0100

    custom stopword list, helper functions, improvements to custom tokenizer, errors correct elsewhere in code (and lots uncommented out!)

--- PartTwo.py
+++ PartTwo.py
@@ -1,10 +1,12 @@
 import pandas as pd
+#pd.set_option('display.max_columns', None)   -- used for custom tokenizer stopword decisions
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, f1_score
 from sklearn import svm
 import spacy
+from collections import Counter
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -53,7 +55,7 @@ random seed of 26.
 def traintestsplitter(df):
     speechtrain, speechtest, partytrain, partytest = train_test_split(df['speech'], df['party'], random_state = 26, stratify = df['party'])
     return speechtrain, speechtest, partytrain, partytest
-
+'''
 def vectorize1(trainX, testX):
     vectorizer = TfidfVectorizer(stop_words = 'english', max_features = 3000)
     trainXvectors = vectorizer.fit_transform(trainX)
@@ -78,7 +80,7 @@ def classifierresults(testY, predY, classifiername):
     print(f'Classification Report for {classifiername}')
     print(classification_report(testY, predY))
     
-def resultreporter(df, vectorizerfunction):    
+def resulttester(df, vectorizerfunction):    
     trainX, testX, trainY, testY = traintestsplitter(df)
     trainXvectors, testXvectors = vectorizerfunction(trainX, testX)
     rfpreds = randomforest(trainXvectors, trainY, testXvectors)
@@ -86,7 +88,7 @@ def resultreporter(df, vectorizerfunction):
     classifierresults(testY, rfpreds, 'Random Forest Classifier')
     classifierresults(testY, svmpreds, 'SVM')
 
-resultreporter(df5, vectorize1)
+resulttester(df5, vectorize1)
 
 def vectorize2(trainX, testX):
     vectorizer = TfidfVectorizer(stop_words = 'english', max_features = 3000, ngram_range = (1, 3))
@@ -95,16 +97,41 @@ def vectorize2(trainX, testX):
     return trainXvectors, testXvectors
 
 resultreporter(df5, vectorize2)
+'''
 
 def customtokenizer(text):
-    #lemmatization ; filter out short tokens OR stopwords ; 
     doc = nlp(text)
     importanttokens = []
+    customstopwords = ['support', 'bill', 'time', 'say', 'know', 'want', 'come', 'place', 'good', 'today', 'amendment', 'debate', 'way', 'ensure', 'issue', 'public', 'think', 'take', 'include', 'change']
     for token in doc:
-        if not token.is_stop:
-            if not token.is_punct:
-                if len(token) > 2:
-                    if token.is_alpha:
-                        importanttokens.append(token.lemma_.lower())
+        if not token.is_stop and len(token) > 2 and token.is_alpha and token.lemma_.lower() not in customstopwords:
+            importanttokens.append(token.lemma_.lower())
     return importanttokens
 
+def toptokenfinder(tokenlist):
+    top30 = Counter(tokenlist).most_common(30)
+    top30tokens = []
+    for pair in top30:
+        top30tokens.append(pair[0])
+    return top30tokens
+
+def stopwordchecker(tokenlist, df):
+    partiestopwords = pd.DataFrame(index = df.index)
+    for token in tokenlist:
+        partiestopwords[token] = df['speech'].str.lower().str.count(token)
+    partiestopwords['party'] = df['party']
+    totalwordseach = df.groupby('party')['speech'].apply(lambda x: x.str.split().str.len().sum())
+    stopwordreport = partiestopwords.groupby('party').sum()
+    normalisedstopwordreport = stopwordreport.div(totalwordseach, axis = 0) * 1000
+    return normalisedstopwordreport
+
+def vectorize3(trainX, testX):
+    vectorizer = TfidfVectorizer(stop_words = 'english', max_features = 3000, ngram_range = (1, 3), tokenizer = customtokenizer)
+    trainXvectors = vectorizer.fit_transform(trainX)
+    testXvectors = vectorizer.transform(testX)
+    return trainXvectors, testXvectors
+#code below was used to identify suitable stopwords for my custom tokenizer
+#allspeechtokens = df5['speech'].apply(customtokenizer).sum()
+#toptokens = toptokenfinder(allspeechtokens)
+#stopwordcheck = stopwordchecker(toptokens, df5)
+#print(stopwordcheck)
